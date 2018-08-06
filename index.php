5f6b68757eb6c1044a06d661c38a876cc6c414ee
rodrigoxxman <rodrigo.salas@tecsup.edu.pe>
2018-08-06 13:11:06 -0500

    primer commit rama rs

--- index.php
+++ index.php
@@ -16,26 +16,30 @@
         <link rel="stylesheet" href="css/bootstrap.css">
          <link rel="stylesheet" href="css/bootstrap1.css">
         <link rel="stylesheet" href="css/estilos.css">
-         
-      
+        <link rel="stylesheet" type="text/css" href="css/estiloMapa.css">
+
+
+
+
+
 
 
 
         <title>Grupo Bantek S.A.C</title>
       </head>
   <body>
-  
+
  <section class="bienvenidos"id="encabezado">
-          
+
      <header class="encabezado navbar fixed-top fondo-menu" role="banner" >
        <div class="container">
               <a href="index.html">
                   <img src="images/logo_bantek.png" alt="Logo del sitio">
               </a>
-              
+
               <button type="button" class="boton-menu hidden-md-up" data-toggle="collapse" data-target="#menu-principal" aria-expanded="false"><i class="fa fa-bars"></i></button>
-             
-              
+
+
               <nav class="collapse" id="menu-principal">
                   <ul>
                       <li class="active"><a data-scroll href="#nosotros" >NOSOTROS</a></li>
@@ -47,23 +51,23 @@
               </nav>
         </div>
      </header>
-          
+
               <div class="texto-encabezado text-center ">
                  <div class="container">
                   <h1 class="text-center display-4 ">Toda nuestra organización enfocada en su seguridad.</h1>
                   <a href="#contacto" class=" btn btn-primary btn-lg ">Contactenos</a>
                  </div>
               </div>
-      
+
               <div class=" text-center flecha-baja ">
                   <a data-scroll href="#nosotros"><i class="fa fa-angle-down"></i></a>
               </div>
 </section>
-     
+
 <section class="quienes-somos container py-3" id="nosotros">
          <div class="container divisor">
-             
-             
+
+
              <div class="row">
                 <div class="col-12 col-sm-6 ">
                     <h2 class="text-center h3 titulo_azul">Nuestro compromiso es optimizar al máximo la inversión de nuestros clientes</h2><br/>
@@ -73,21 +77,21 @@
              <p class="text-justify">Además de esto, estamos asociados estrategicamente con los principales fabricantes a nivel mundial de equipos y soluciones que comercializamos.</p>
                  </div>
              </div>
-             
+
          </div>
  </section>
- 
-    
+
+
 <section class="soluciones-infraestructura soluciones-centrado container divisor py-3" data-scroll id="servicios">
-         
+
          <div class="container hidden-sm-up py-4">
           <h2> Soluciones de infraestructura</h2>
           </div>
 <div class="container">
         <div class="row">
-        
 
-            
+
+
         <ul class="col-12 col-sm-8 " >
             <li >
                <div class="contenedor-infraestructura py-3">
@@ -100,9 +104,9 @@
                   <p>Balanceo</p>
                   <p>Routing</p>
 
-                  
-                  
-                  
+
+
+
             </div>
             </li>
                <li class="cableado">
@@ -111,15 +115,15 @@
                   <p>Electricidad</p>
                   <p>Datos</p>
                   <p>Voz</p>
-                
+
                 </div>
               </li>
-              
+
                 <li class="equipamiento">
-              
-               <div class="contenedor-infraestructura py-3"> 
+
+               <div class="contenedor-infraestructura py-3">
                   <h4>Equipamiento</h4>
-                 
+
                   <p>Equipos de escritorio</p>
                   <p>Control de accesos</p>
                   <p>Equipos portátiles</p>
@@ -127,59 +131,59 @@
                   <p>Optimización</p>
                   <p>Servidores</p>
                   <p>Balanceo</p>
-                  
-                  
-                  
+
+
+
                 </div>
             </li>
-            
+
               <li class="software">
                 <div class="contenedor-infraestructura">
                 <h4>Software Varios</h4>
                   <p>Ofimática</p>
                   <p>Otros</p>
-                  </div>   
+                  </div>
             </li>
-        
+
         </ul>
-      
+
                 <div class=" hidden-xs-down col-sm-4 text-center  ">
-               
-               
-              
+
+
+
               <h2 class="titulo">Soluciones de infraestructura</h2>
-              
+
           </div>
-               
-        
-        
+
+
+
        </div>
-       
+
  </div>
-   
-   
-   
-   
-   
+
+
+
+
+
 
    </section>
-    
+
   <main class="soluciones-seguridad .soluciones-derecha container divisor py-3">
-                
+
          <div class="container hidden-sm-up py-4 text-center">
           <h2>Soluciones de seguridad</h2>
           </div>
 <div class="container ">
         <div class="row">
-        
+
           <div class=" hidden-xs-down col-sm-5 text-left  ">
-               
-               
-              
+
+
+
               <h2 class="titulo">Soluciones de Seguridad</h2>
-              
+
           </div>
-        
+
         <ul class="col-12 col-sm-7 seguridad py-3" >
             <li >
                <div class="contenedor-seguridad py-3">
@@ -190,7 +194,7 @@
                   <p>Antimalware</p>
                   <p>WAF</p>
                   <p>Análisis de vulnerabilidades</p>
-                  
+
             </div>
             </li>
                <li >
@@ -201,11 +205,11 @@
                   <p>DLP</p>
                   <p>Antimalware</p>
                   <p>Backup de Estaciones</p>
-                  </div>  
-               
+                  </div>
+
               </li>
                      <li >
-              
+
               <div class="contenedor-seguridad py-3">
                 <h4>Data Center y Red</h4>
                   <p>Database Activity Monitoring (DAM)</p>
@@ -214,38 +218,38 @@
                   <p>DLP de Red</p>
                    <p>Visibilidad y Control de la Red</p>
                 </div>
-               
+
             </li>
-            
+
               <li >
-                 <div class="contenedor-seguridad py-3"> 
+                 <div class="contenedor-seguridad py-3">
                 <h4>Cloud</h4>
                   <p>CASB</p>
                   <p>Federación</p>
                   <p>Canal Electrónico</p>
-                
+
                 </div>
             </li>
-        
+
         </ul>
-        
-       
-        
-               
-               
-              
-             
-        
+
+
+
+
+
+
+
+
        </div>
-       
+
  </div>
 
-       
-    
-        
+
+
+
     </main>
-    
-    
+
+
    <section class="servicios-de-seguridad soluciones-centrado container divisor py-3  ">
 
            <div class="container hidden-sm-up py-4 titulo_azul">
@@ -253,9 +257,9 @@
           </div>
 <div class="container">
         <div class="row">
-        
-        
-        
+
+
+
         <ul class="col-12 col-sm-8 " >
             <li >
                <div class="contenedor-seguridad py-3">
@@ -264,8 +268,8 @@
                   <p>Servicios gestionados de seguridad 24/7</p>
                   <p>Servicios administrados de seguridad 24/7</p>
                   <p>Servicios de soporte técnico 24/7</p>
-            
-                  
+
+
             </div>
             </li>
                <li >
@@ -273,32 +277,32 @@
                 <h4>Cloud</h4>
                   <p>Servicios de implementación y configuración de plataforma</p>
                   <p>Servicios de monitoreo de seguridad 24/7</p>
-                
+
                 </div>
               </li>
-              
+
                   <li >
-              
+
               <div class="contenedor-seguridad py-3">
                 <h4>Endpoint</h4>
                   <p>Servicios de implementación y configuración de plataforma</p>
                   <p>Servicios gestionados de seguridad 24/7</p>
                   <p>Servicios administrados de seguridad 24/7</p>
                   <p>Servicios de soporte técnico 24/7</p>
-                  </div> 
-              
+                  </div>
+
             </li>
-            
+
               <li >
-                   <div class="contenedor-seguridad py-3"> 
+                   <div class="contenedor-seguridad py-3">
                 <h4>Datacenter</h4>
                   <p>Servicios de implementación y configuración de plataforma</p>
                   <p>Servicios de monitoreo de seguridad 24/7</p>
-                 
-                
+
+
                 </div>
             </li>
-            
+
               <li >
                 <div class="contenedor-seguridad">
                 <h4>Consultoría</h4>
@@ -307,174 +311,178 @@
                   <p>Auditoría y Control</p>
                   <p>Externalización de Servicios</p>
                   <p>Entrenamiento de Seguridad de la Información</p>
-                  </div>   
+                  </div>
             </li>
-        
+
         </ul>
-      
+
      <div class=" hidden-xs-down col-sm-4 text-right  ">
-               
-               
-              
+
+
+
               <h2 class="titulo titulo_azul"> Servicios de Seguridad</h2>
-              
+
           </div>
-              
-        
+
+
        </div>
-       
+
  </div>
- 
-      
+
+
    </section>
-   
-   
+
+
     <section class="container nuestros-aliados  divisor py-4 text-center" data-scroll id="aliados">
-       
-       
-       
+
+
+
         <div class="container text-center py-4">
           <h2>Nuestros Aliados</h2>
           </div>
 <div class="container">
         <div class="row">
-        
-        
-        
+
+
+
         <ul class="col-3 col-sm-3 ">
             <li>
                <div class="aliados-img">
                 <img src="images/Cisco_logo.svg" alt="">
-                 
-            
-                  
+
+
+
             </div>
             </li>
                <li >
                 <div class="aliados-img">
                 <img src="images/hp.png" alt="">
-                 
-            
-                  
+
+
+
             </div>
               </li>
-        
-        
+
+
         <li >
                 <div class="aliados-img">
                 <img src="images/symantec.png" alt="">
-                 
-            
-                  
+
+
+
             </div>
               </li>
         </ul>
-      
+
        <ul class="col-3 col-sm-3 ">
             <li>
                <div class="aliados-img">
                 <img src="images/mcafee.png" alt="">
-                 
-            
-                  
+
+
+
             </div>
             </li>
                <li >
                 <div class="aliados-img">
                 <img src="images/barracuda.png" alt="">
-                 
-            
-                  
+
+
+
             </div>
               </li>
-        
-        
+
+
         <li >
                 <div class="aliados-img">
                 <img src="images/palo-alto-networks.png" alt="">
-                 
-            
-                  
+
+
+
             </div>
               </li>
         </ul>
-        
+
         <ul class="col-3 col-sm-3 ">
             <li>
                <div class="aliados-img">
                 <img src="images/fortinet.png" alt="">
-                 
-            
-                  
+
+
+
             </div>
             </li>
                <li >
                 <div class="aliados-img">
                 <img src="images/f5.png" alt="">
-                 
-                        
 
-                  
+
+
+
             </div>
               </li>
-        
-        
+
+
         <li >
                 <div class="aliados-img">
                 <img src="images/crowdstrike.png" alt="">
-                 
-            
-                  
+
+
+
             </div>
               </li>
         </ul>
-       
+
        <ul class="col-3 col-sm-3 ">
             <li>
                <div class="aliados-img">
                 <img src="images/fireeye.svg" alt="">
-                 
-            
-                  
+
+
+
             </div>
             </li>
                <li >
                 <div class="aliados-img">
                 <img src="images/cyber-ark.svg" alt="">
-                 
-            
-                  
+
+
+
             </div>
               </li>
-        
-        
+
+
         <li >
                 <div class="aliados-img">
                 <img src="images/cirix.png" alt="">
-                 
-            
-                  
+
+
+
             </div>
               </li>
         </ul>
-       
+
        </div>
-       
-       
-       
+
+
+
  </div>
-       
-       
-       
-       
-       
-      
-    
+
+
+
+
+
+
+
     </section>
-    
-    
-    
-    
+
+
+
+
+
+
+
+
     <section class="contactenos py-4 " data-scroll id="contacto">
         <main class="py-1">
         <div class="container ">
@@ -484,11 +492,9 @@
                     <h2 class="m-b-2">Contáctenos</h2>
 
 
-                    <form action="#">
+                    <form action="#" method="post">
 
                         <div class="form-group row">
-                            
-
                             <div class="col-md-12">
                                <label for="nombre" class="col-md-4 col-form-label">Nombre</label>
                                 <input class="form-control" type="text" id="nombre" name="nombre" placeholder="Ingrese su nombre" data-toggle="tooltip" data-placement="top" title="Ingrese su nombre completo">
@@ -497,7 +503,7 @@
 
 
                         <div class="form-group row">
-                           
+
 
                             <div class="col-md-12">
                                 <label for="email" class="col-md-4 col-form-label">Email</label>
@@ -505,16 +511,16 @@
                             </div>
                         </div>
                                <div class="form-group row">
-                           
+
 
                             <div class="col-md-12">
-                                <label for="email" class="col-md-4 col-form-label">Telefono</label>
-                                <input class="form-control" type="text" id="telefono" name="email" placeholder="Ingrese su numero" data-toggle="tooltip" data-placement="top" title="Ingrese su email">
+                                <label for="telefono" class="col-md-4 col-form-label">Telefono</label>
+                                <input class="form-control" type="text" id="telefono" name="telefono" placeholder="Ingrese su numero" data-toggle="tooltip" data-placement="top" title="Ingrese su email">
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            
+
 
                             <div class="col-md-12">
                                <label for="mensaje" class="col-md-4 col-form-label">Mensaje</label>
@@ -536,45 +542,68 @@
 
 
                 </div>
+
+                <!--Seccion Google Maps-->
                 <div class="container col-md-6 ">
                     <h3>¿Dónde estamos?</h3>
-                    
+
                     <div class="container-fluid">
-                        
-                        <img src="images/maps.png" alt="">
+                      <!--Codigo html Mapa-->
+                      <strong>Grupo Bantek</strong>/ Latitud: -12.126109 / Longitud: -77.020603 / Zoom: 18
+                      <div id="mapaBantek" ></div>
+                      <!--Codigo js Mapa-->
+                      <script>
+
+                      function initialize() {
+                        var localizacionbantek={lat: -12.126109, lng: -77.020603};
+                        var mapa = document.getElementById('mapaBantek');
+                        var mapOptions = {
+                          center: new google.maps.LatLng(-12.126109, -77.020603),
+                          zoom: 18,
+                          mapTypeId: google.maps.MapTypeId.ROADMAP
+                        }
+                        var mapa = new google.maps.Map(mapa, mapOptions)
+
+                        //icono del mapa
+                        var marker = new google.maps.Marker({
+                          position: localizacionbantek,
+                          map: mapa,
+                          title: 'Bantek'
+                        });
+
+                      }
+                      google.maps.event.addDomListener(window, 'load', initialize);
+
+                    </script>
                     </div>
-                    
-                    
+
+
                     <div class="container">
                     <div class="row ">
                   <ul class="col-12" >
                       <li ><p> <i class="fa fa-globe" aria-hidden="true"> </i> Av. Benavides 1180 Of. 402 - Miraflores, Lima - Perú</p> </li>
                       <li ><p><i class="fa fa-phone" aria-hidden="true"> </i> (511) 01 445 5997</p> </li>
                       <li ><p><i class="fa fa-inbox" aria-hidden="true"> </i> info@grupobantek.com.pe</p> </li>
-                  </ul>      
+                  </ul>
                    </div>
                     </div>
-                                         
-                    
-               
-
                     </div>
-                   
-    
+
+
                 </div>
 
 
 
 
 
-           
+
         </div>
     </main>
 
-</section>             
-    
-    
-    
+</section>
+
+
+
      <footer class="piedepagina py-1" role="contentinfo" data-scroll id="pie-pagina">
         <div class="container">
             <p>2018 © Grupo Bantek S.A.C Todos los derechos reservados</p>
@@ -582,26 +611,29 @@
                 <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"> </i>  </a></li>
                 <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i> </a></li>
                 <li><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i> </a></li>
-                
-                
+
+
             </ul>
 
         </div>
 
     </footer>
 
-    
+
     <a data-scroll="" class="ir-arriba" href="#encabezado" style="display: block;"><i class="fa  fa-arrow-circle-up" aria-hidden="true"> </i> </a>
     <!-- Carga JavaScript -->
     <script src="js/jquery-1.9.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/smooth-scroll.min.js"></script>
     <script src="js/sitio.js"></script>
-    
- 
+
+
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-   
-   
-   
+
+
+    <!--Resource api Google-->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIGqOtA3Q9UB28ckkbfHiheV6Q6qa56fk&callback=initialize"></script>
+
+
   </body>
 </html>
